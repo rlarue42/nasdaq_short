a0cd664b6b64df0bf324686957d83061aabb8f4a
Michal Kapusta <Michal@mks-macbook.intern>
2018-04-19 09:59:31 +0200

    first batch

---
+++ 01_main.R
@@ -0,0 +1,15 @@
+library(tidyquant)
+library(tidyverse)
+library(ggplot2)
+library(quantmod)
+library(rebus)
+
+
+#devtools::install_github("business-science/tidyquant")
+
+# launch scripts ----------------------------------------------------------
+
+# source(file = "_R/02_source_data.R") run only if need to source new data!
+source(file = "_R/03_analyze_data.R")
+
+
---
+++ _R/02_source_data.R
@@ -0,0 +1,38 @@
+library(tidyquant)
+library(tidyverse)
+library(ggplot2)
+library(quantmod)
+library(rebus)
+
+#devtools::install_github("business-science/tidyquant")
+
+
+# define stock universum --------------------------------------------------
+
+lst_get_option <- tq_get_options()
+lst_nasdaq <- tq_exchange("NASDAQ")
+
+# reduce investable universe to +500 market cap & sector Technology 
+chrkt <- "\\$" 
+
+lst_nasdaq_filtered <- lst_nasdaq %>% 
+        filter(sector == "Technology") %>% 
+        mutate(market.cap = str_replace_all(market.cap, "M",""),
+               market.cap = str_replace_all(market.cap, "B",""),
+               market.cap = str_replace_all(market.cap, chrkt,""),
+               market.cap = as.integer(market.cap)) %>% 
+        filter(market.cap > 500) # now just 47 stocks!
+
+list_stock <- lst_nasdaq_filtered %>% pull(symbol) %>% unique()
+
+# retrive nasdaq tradet stock  --------------------------------------------
+
+df_database   <- map(tq_get,.x = list_stock, get = c("key.ratios","stock.prices"))
+df_key_ratios <- df_database %>% map(unnest,key.ratios) %>% map_df(unnest,data) 
+df_stock_data <- df_database %>% map_df(unnest,stock.prices) %>% gather(ratio, value,3:length(.))
+
+
+# store the database ------------------------------------------------------
+
+write_csv(df_key_ratios, path = paste0("_raw_data/df_key_ratios_",Sys.Date(),".csv"))
+write_csv(df_stock_data, path = paste0("_raw_data/df_stock_data_",Sys.Date(),".csv"))
