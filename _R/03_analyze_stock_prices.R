179d82e9cc22f3fc6ab124e2088bd700d7a7ed7c
Michal Kapusta <Michal@MKs-MacBook.local>
2018-05-01 16:45:00 +0200

    work on "Valuation based filter algorithm" VBFA

--- _R/03_analyze_stock_prices.R
+++ _R/03_analyze_stock_prices.R
@@ -11,12 +11,6 @@ df_stock_data <-  read_csv(file = "_raw_data/df_stock_data_2018-04-19.csv")
 
 
 
-# create lists ------------------------------------------------------------
-
-df_stock_data %>% colnames()
-df_stock_data$ratio %>% unique()
-
-
 # from long to  wide format 
 to_wide <- function(data, rt) {
         data %>%
@@ -27,7 +21,7 @@ to_wide <- function(data, rt) {
 }
 
 
-# analyze  stock prices -----------------------------------------------------------
+# plot stock prices -----------------------------------------------------------
 
 # create chart databand
 chart_ts_bb <- function(data, stock.ticker) {
@@ -44,9 +38,63 @@ chart_ts_bb <- function(data, stock.ticker) {
                 theme_minimal()
 }
 
-
-
 # Plot stock price & bbands
 chart_ts_bb(data = df_stock_data, stock.ticker = c("BBSI"))
 chart_ts_bb(data = df_stock_data, stock.ticker = c("AAPL","VNET"))
 
+
+# find peer group outperforming stocks  -----------------------------------
+
+df_sma <- df_stock_data %>%
+        filter(ratio == "adjusted") %>% 
+        split(.$symbol) %>% 
+        map(drop_na) %>% 
+        map(safely(mutate),sma30 = SMA(value,n = 30),
+                           sma90 = SMA(value,n = 90),
+                           sma180 = SMA(value,n = 180),
+                           sma360 = SMA(value,n = 360),
+                           sma1000 = SMA(value,n = 1000),
+            trend_30 = case_when(sma30 > value ~ "below",
+                                 sma30 < value ~ "above"),
+            trend_90 = case_when(sma90 > value ~ "below",
+                                 sma90 < value ~ "above"),
+            trend_180 = case_when(sma180 > value ~ "below",
+                                  sma180 < value ~ "above"),
+            trend_360 = case_when(sma360 > value ~ "below",
+                                  sma360 < value ~ "above"),
+            trend_1000 = case_when(sma1000 > value ~ "below",
+                                  sma1000 < value ~ "above")) %>% 
+        map_df("result")
+
+
+df_elevated <- df_sma %>%
+        split(.$symbol) %>%
+        map(tail, n = 1) %>% 
+        map_df(filter, trend_1000 == "above", trend_30 == "above")  %>% 
+        select(symbol) %>%
+        pull()
+
+
+chrts <- df_sma %>%
+                filter(symbol %in% df_elevated) %>%  
+                select_at(c(1:2,4:9)) %>% 
+                gather(ratio,value,3:length(.)) %>% 
+                split(.$symbol) %>% 
+                map( ~
+                ggplot(data = . , 
+                       aes(date, value, col = ratio)) +
+                geom_line() +
+                facet_wrap(~symbol, scales = "free") +
+                theme_minimal() +
+                theme(legend.position = "bottom") + 
+                scale_color_tq() + 
+                        labs(title = paste0(.$symbol),
+                             subtitle = "above 360 & 30 day SMA"))  
+                
+
+chrts[[19]]
+
+tq_mutate_fun_options()
+
+
+# stoch
--- _R/03_analyze_stock_ratios.R
+++ _R/03_analyze_stock_ratios.R
@@ -13,7 +13,6 @@ df_key_ratios <-  read_csv(file = "_raw_data/df_key_ratios_2018-04-19.csv")
 
 lst_sections       <- df_key_ratios %>% pull(section) %>% unique()
 lst_sub_sections   <- df_key_ratios %>% pull(sub.section) %>% unique()
-
 lst_stocks         <- df_key_ratios %>% pull(symbol) %>% unique()
 
 
@@ -30,10 +29,16 @@ df_key_ratios_wide <- map(to_wide, .x = lst_key_ratios, data = df_key_ratios)
 
 # analyze  stock key ratio's ------------------------------------------------------
 
+
+# extract sections
+df_val <-  df_key_ratios %>%
+        select(-group) %>%
+        filter(section == "Valuation Ratios") 
+
 df_financials <- df_key_ratios %>%
                  select(-group) %>%
                 filter(section == "Financials") %>%
-                split(.$sub.section) %>% 
+                split(.$category) %>% 
                 map_df(spread, category, value)
 
 df_profitability <- df_key_ratios %>%
@@ -42,20 +47,60 @@ df_profitability <- df_key_ratios %>%
                 split(.$sub.section) %>% 
                 map(spread, category, value)
 
-df_profitability %>% map(Profitability)
+# valuation based filter algorithm (VBFA)
+
+calc_stock_valuation_stats <- function(data) {
+        data <- data %>% 
+                group_by(category) %>%
+                summarise(last = last(value),
+                          year_ago = nth(value,-1),
+                          mean = mean(value,na.rm = T)) %>% 
+                ungroup()
+        
+        
+}
+
+calc_index_valuation_stats <- function(data) {
+        data <- data %>% 
+                group_by(date) %>%
+                summarise(index_median = median(value,na.rm = T)) %>%
+                drop_na()
+        return(data)
+}
 
 
-stats <- df_financials %>% select_if(is.numeric) %>% skimr::skim()
 
 
+df_val_alg_stock <- df_val %>%
+                        nest(-symbol) %>%
+                        mutate(summary = map(data, calc_stock_valuation_stats))
+
+
+df_val_alg_index <- df_val %>%
+                        nest(-category) %>%
+                        mutate(summary = map(data, calc_index_valuation_stats))
+
+                
+df_val_alg_final <- df_val_alg_stock %>%
+        unnest(summary) %>%
+        left_join(df_val_alg_index %>%
+                         unnest(summary) %>%
+                         filter(date == "2017-12-29")) %>% 
+        select(symbol, category, last, index_median) %>% 
+        group_by(category) %>% 
+        mutate(to_index = last / index_median,
+               to_index_rank = ntile(to_index,10)) %>% 
+        ungroup() %>% 
+        group_by(symbol) %>% 
+        mutate(val_alg_score = sum(to_index_rank)) %>% 
+        arrange(-val_alg_score)
+        
 
 # goals! ------------------------------------------------------------------
 
 *Most leveraged (short term)
 *Declining sales
 *stock prices above trend
-
-
 
 
 # actionable steps --------------------------------------------------------
